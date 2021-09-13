2be5f60c36846e059ce2e060fe949608de7a24a4
Dmytro <dimaniko04@ukr.net>
2021-09-13 18:18:56 +0300

    Implemented modal window and styles to it. Added resetList mixin. Slightly improved the code in Burgers/index.js.

--- src/components/Burgers/index.tsx
+++ src/components/Burgers/index.tsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { BurgersListProps } from "../../shared/types/Props";
 import styles from "./Burgers.module.scss";
 import { BurgersLoader } from "./BurgerLoader";
+import { OrderModal } from "./../OrderModal/index";
+import { Burger } from "../../shared/types/Burgers";
+
+const Loader = () => (
+  <>
+    {[...new Array(10)].map((_, index) => (
+      <li key={index} className={styles.burger}>
+        <BurgersLoader />
+      </li>
+    ))}
+  </>
+);
 
 export const BurgersList = ({ burgers }: BurgersListProps) => {
+  const [selectedBurger, setSelectedBurger] = useState<Burger | null>(null);
+
+  const handleClick = (burger: Burger) => setSelectedBurger(burger);
+  const closeModal = () => setSelectedBurger(null);
+
   return (
     <ul className={styles.burgersList}>
-      {burgers.length
-        ? burgers.map((burger) => (
-            <li key={burger._id} className={styles.burger}>
-              <img src={burger.img} alt="Crush" />
-              <span className={styles.burgerName}>{burger.name}</span>
-            </li>
-          ))
-        : [...new Array(10)].map((_, index) => (
-            <li key={index} className={styles.burger}>
-              <BurgersLoader />
-            </li>
-          ))}
+      <OrderModal burger={selectedBurger} closeModal={closeModal} />
+      {burgers.length ? (
+        burgers.map((burger) => (
+          <li
+            key={burger._id}
+            className={styles.burger}
+            onClick={() => handleClick(burger)}
+          >
+            <img src={burger.img} alt="Crash" />
+            <span className={styles.burgerName}>{burger.name}</span>
+          </li>
+        ))
+      ) : (
+        <Loader />
+      )}
     </ul>
   );
 };
