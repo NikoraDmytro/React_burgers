ecc8c36e72e5b3292653bbcebeb997b6516ed3e1
Dmytro <dimaniko04@ukr.net>
2021-10-03 14:17:19 +0300

    Moved some styles for buttons  into shared/styles/components folder to make them reusable.

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import styles from "./Cart.module.scss";
 import { Link } from "react-router-dom";
 import { CartList } from "./../../components/CartList/index";
+import { useTypedSelector } from "../../store/hooks";
 
 export const CartPage = () => {
+  const totalQuantity = useTypedSelector((state) => state.cart.quantity);
+
   return (
     <div className={styles.cart}>
       <Link className={styles.goBack} to="/">
         <span>{"\u2190"}</span> Go back
       </Link>
       <h1 className={styles.title}>Your order</h1>
+      <button className={styles.makeOrderButton}>
+        Order products ({totalQuantity})
+      </button>
       <CartList />
     </div>
   );
